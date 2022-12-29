3800c8a8a0e1e6aa6505ac3eed12681fe0484f84
Артур Шодиев <shah-fire@mail.ru>
2022-12-29 12:43:01 +0300

    Переписал ArrayUserObject с применением Stream API

--- src/main/java/org/example/ArrayUserGenerator.java
+++ src/main/java/org/example/ArrayUserGenerator.java
@@ -1,50 +1,38 @@
 package org.example;
 
-import net.bytebuddy.implementation.bind.annotation.Default;
-import org.apache.commons.lang3.ArrayUtils;
-
-import javax.naming.Name;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ArrayUserGenerator {
-
-
-    private static User generateUser(String[] names, int minAge, int maxAge){
+    @FunctionalInterface
+    public interface Supplier<T> extends java.util.function.Supplier<T> {
+        T get();
+    }
+    private static Supplier<User> generateUser = () -> {
         String id = StringGenerator.generateLatin(11);
+        String[] names =  NameArchive.getAny();
         String name = names[Randomizer.randomWithRange(0, names.length - 1)];
-        int age = Randomizer.randomWithRange(minAge, maxAge);
+        int age = Randomizer.randomWithRange(16, 99);
         return new User(id, name, age);
+    };
+    public static List<User> generateUsers(int sizeStream){
+        Stream stream = Stream.generate(generateUser).limit(sizeStream);
+        return stream.toList();
     }
-
-    public static ArrayList<User> generateUsers(int amount, String[] nameGender, int minAge, int maxAge){
-        ArrayList<User> array = new ArrayList<User>();
-        for (int i = 0; i < amount; i++){
-            array.add(generateUser(nameGender, minAge, maxAge));
-        }
-        return array;
+    public static List<User> ageFilter(List<User> list, int minAge, int maxAge){
+        List<User> filtered = list.stream().filter(p -> p.getAge() < maxAge).filter(p -> p.getAge() > minAge).collect(Collectors.toList());
+        Stream filterStream = filtered.stream();
+        return filterStream.toList();
     }
-//    public static void generateAndPrintUsers(int amount){
-//        ArrayList<User> array = new ArrayList<User>();
-//        array.addAll(generateUsers(amount, NameArchive.getAny(), 16, 99));
-//        for (int i = 0; i < amount; i++){
-//            printUserInfo(array, i);
-//        }
-//    }
-//    public static void generateAndPrintUsers(int amount, String[] nameGender){
-//        ArrayList<User> array = new ArrayList<User>();
-//        array.addAll(generateUsers(amount, nameGender, 16, 99));
-//        for (int i = 0; i < amount; i++){
-//            printUserInfo(array, i);
-//        }
-//    }
-    public static void generateAndPrintUsers(int amount, String[] nameGender, int minAge, int maxAge){
-        ArrayList<User> array = new ArrayList<User>();
-        array.addAll(generateUsers(amount, nameGender, minAge, maxAge));
-        Stream stream = array.stream();
-        stream.forEach(System.out::println);
+    public static List<User> nameFilter(List<User> list){
+        List<String> maleList = Arrays.asList(NameArchive.getMale());
+        List<User> filtered = list.stream().filter(p -> maleList.contains(p.getName())).collect(Collectors.toList());
+        Stream filterStream = filtered.stream();
+        return filterStream.toList();
     }
-//    public static void printUserInfo(ArrayList<User> array, int index){
-//        System.out.println(array.get(index).toString());
-//    }
+    public static void printlnUsers(List<User> list){
+        list.stream().toList().forEach(System.out::println);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-//        ArrayUserGenerator.generateAndPrintUsers(10);
-//        ArrayUserGenerator.generateAndPrintUsers(10, NameArchive.getMale());
-        ArrayUserGenerator.generateAndPrintUsers(10, NameArchive.getAny(), 50, 99);
+          List<User> users = ArrayUserGenerator.generateUsers(3);
+          ArrayUserGenerator.printlnUsers(users);
+          List<User> filterUsers = ArrayUserGenerator.ageFilter(users, 50, 100);
+          ArrayUserGenerator.printlnUsers(filterUsers);
+          List<User> arrayList = ArrayUserGenerator.nameFilter(users);
+        ArrayUserGenerator.printlnUsers(arrayList);
+
     }
 }
